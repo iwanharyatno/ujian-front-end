3f770dc8f8c3266e110e3d23be371ef062c8d792
iwanharyatno <iwnharry61@gmail.com>
2023-01-14 11:43:16 +0700

    Membuat efek transisi keluar dan masuk

--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -1,11 +1,9 @@
 export default function OffCanvas(props) {
-  const displayClass = props.open ? 'block' : 'hidden';
-
   const handleClick = event => event.stopPropagation();
 
   return (
-    <div className={'z-40 fixed top-0 left-0 w-full h-full bg-black/60 ' + displayClass} onClick={props.onOverlayClick}>
-      <div className={'fixed w-full bottom-0 rounded-t-3xl ' + displayClass + ' ' + props.colors} onClick={handleClick}>
+    <div className={'z-40 fixed top-0 left-0 w-full h-full bg-black/60 ' + (props.open ? 'h-full' : 'h-0' )} onClick={props.onOverlayClick}>
+      <div className={'fixed w-full bottom-0 rounded-t-3xl transition-transform ' + (props.open ? 'translate-y-0' : 'translate-y-full' ) + ' ' + props.colors} onClick={handleClick}>
         {props.children}
       </div>
     </div>
