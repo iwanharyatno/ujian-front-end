cb9d6e0860144e0d27fdaab67084f204fffcc3b5
iwanharyatno <iwnharry61@gmail.com>
2023-01-14 11:44:38 +0700

    Menambahkan endpoint delete kelas

--- src/api/endpoints/kelas.js
+++ src/api/endpoints/kelas.js
@@ -5,7 +5,8 @@ const BASE = AppConfig.BASE_API_URL + '/kelas';
 const KelasEndpoint = {
   GET_ALL: BASE,
   POST: BASE + '/insert',
-  UPDATE: (id) => BASE + `/update/${id}`,
+  UPDATE: (id) => `${BASE}/update/${id}`,
+  DELETE: (id) => `${BASE}/delete/${id}`,
 };
 
 export default KelasEndpoint;
